a3765f10487665509725d0e254b9cae5d0faede4
Elijah Noble <elijahnobledev@gmail.com>
2022-06-16 17:13:28 -0700

    Assignment Completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  // add this callback function
+  function handleUpdateItem(updatedItem) {
+    console.log("In ShoppingCart:", updatedItem);
+  }
+
+  function handleAddToCartClick() {
+    // Call onUpdateItem, passing the data returned from the fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  function Item({ item, onUpdateItem, onDeleteItem }) {
+    function handleDeleteClick() {
+      // Call onDeleteItem, passing the deleted item
+      fetch(`http://localhost:4000/items/${item.id}`, {
+        method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item));
+  }
+
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      {/* add the onClick listener */}
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
